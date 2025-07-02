ed18f3bce3ddeb4cd099e656891e2eec608ab781
Md. Faishal <85985502+faishal-developer@users.noreply.github.com>
2025-07-02 18:11:35 +0600

    get misisdn

--- src/middleware.js
+++ src/middleware.js
@@ -9,7 +9,7 @@ export async function middleware(req) {
   const method = req.method;
   // const redirectUrlFullPath = "http://localhost:3000/unauthorized";
   const redirectUrlFullPath = "https://myblaudiobook.kabbik.com/";
-  let msisdn = req.headers.get('x-msisdn') || req.headers.get('X-MSISDN') || req.headers.get('msisdn') ||  req.headers.get('x-access-number') || req.nextUrl?.searchParams.get('number')
+  let msisdn = req.headers.get('x-msisdn') || req.headers.get('X-MSISDN') || req.headers.get('msisdn') || req.headers.get('MSISDN') || req.headers.get('x-access-number') || req.nextUrl?.searchParams.get('number')
   if(msisdn){
     return NextResponse.redirect(`https://myblaudiobook.kabbik.com/?msisdn=${msisdn}`);
   }else {
